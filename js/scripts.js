a5762df5d2fd296a43aded108efd6e4cbf5efb4d
spaceled <spaceled@gmail.com>
2020-07-22 22:59:35 +0600

    Tahmina Nasrin Pioneer Bank Done!

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,43 @@
-// Login button event handler
-const loginBtn = document.getElementById('login');
-loginBtn.addEventListener("click", function() {
-    const loginArea = document.getElementById('login-area');
+// login button event handler
+const loginBtn = document.getElementById("login");
+loginBtn.addEventListener("click", function(){
+    const loginArea = document.getElementById("login-area");
     loginArea.style.display = "none";
-    const transactionArea = document.getElementById('transaction-area');
-    transactionArea.style.display = 'block';
+    const transactionArea = document.getElementById("transaction-area");
+    transactionArea.style.display = "block";
 })
-// Deposit button event handler
-const depositBtn = document.getElementById('addDeposit');
-depositBtn.addEventListener('click', function(){
-    const depositNumber = getInputNumber("depositAmount");
-    
-    updateSpanText('currentDeposit', depositNumber);
-    updateSpanText('currentBalance', depositNumber);
 
-    document.getElementById('depositAmount').value = "";
+//deposit button event handler
+const depositBtn = document.getElementById("addDeposit");
+depositBtn.addEventListener("click", function(){
+    const depositNumber = getInputNumber("depositAmount");        
+
+    updateSpanText("currentDeposit", depositNumber);
+    updateSpanText("currentBalance", depositNumber);
+
+    document.getElementById("depositAmount").value = "";
 })
-// Withdraw button event handler
-const withddrawBtn = document.getElementById("addWithdraw");
-withddrawBtn.addEventListener('click', function() {
-    const withdrawNumber  = getInputNumber("withdrawAmount");
+
+// withdraw button event handler
+const withdrawBtn = document.getElementById("addWithdraw");
+withdrawBtn.addEventListener("click", function(){
+    const withdrawNumber = getInputNumber("withdrawAmount");
+    
+    updateSpanText("currentWithdraw", withdrawNumber);
+    updateSpanText("currentBalance", -1 * withdrawNumber);
+
+    document.getElementById("withdrawAmount").value = ""
 })
 
 function getInputNumber(id){
     const amount = document.getElementById(id).value;
-    const amountNumber  = parseFloat(amount);
-    return withdrawNumber;
+    const amountNumber = parseFloat(amount);
+    return amountNumber;
 }
 
-function updateSpanText(id, depositNumber){
+function updateSpanText(id, addedNumber){
     const current = document.getElementById(id).innerText;
     const currentNumber = parseFloat(current);
-    const totalAmount = depositNumber + currentNumber;
+    const totalAmount = addedNumber + currentNumber;
     document.getElementById(id).innerText = totalAmount;
 }
